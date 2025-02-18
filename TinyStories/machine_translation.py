d640b5fa8d7c185aabd88eacd5d01540cd4c066e
Niranjan Thanikachalam <thanikachalam.niranjan@gmail.com>
2025-02-18 10:28:01 +0100

    translate with deep_translator

--- TinyStories/machine_translation.py
+++ TinyStories/machine_translation.py
@@ -1,34 +1,34 @@
-import asyncio
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 from datasets import load_dataset, Dataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-async def translate_bulk(inputs):
-    async with Translator() as translator:
-        translations = await translator.translate(inputs, dest='ta')
-
-    if not isinstance(translations, list):
-        translations = [translations]
-    
-    return [{"text":translation.text for translation in translations}]
+def translate_bulk(inputs):
+    translator = GoogleTranslator(source='en', target='ta')
+    translations = translator.translate_batch(inputs)
+    return [{"text":translation for translation in translations}]
 
 def collate_text(batch):
     return [item["text"] for item in batch]
 
-async def main():
+def save_dataset(dataset_dict, path):
+    dataset = Dataset.from_dict(dataset_dict)
+    dataset.save_to_disk(path)
+
+def main():
     dataset = load_dataset("roneneldan/TinyStories")
-    ta_dataset = {key:[] for key in dataset.keys()}
+    ta_dataset = {}
     for key, split in dataset.items():
-        dataloader = DataLoader(split, batch_size = 50, shuffle = False, collate_fn = collate_text)
-        for batch in  tqdm(dataloader, "Processing data", total=len(dataloader)):
-            translated_batch = await translate_bulk(batch)
+        ta_dataset[key] = []
+        dataloader = DataLoader(split, batch_size = 1000, shuffle = False, collate_fn = collate_text)
+        for index,batch in  tqdm(enumerate(dataloader), "Processing data", total=len(dataloader)):
+            translated_batch = translate_bulk(batch)
             ta_dataset[key].append(translated_batch)
-            
-    ta_tinystories = Dataset.from_dict(ta_dataset)
-    ta_tinystories.save_to_disk("ta_tinystories_google_translate")
+            if index%20 == 0:
+                save_dataset(ta_dataset, "ta_tinystories_google_transdlate")
+    save_dataset(ta_dataset, "ta_tinystories_google_translate")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
 
 
