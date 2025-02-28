422161121099a241f28e8d1ba09877fde47cd99f
Niranjan Thanikachalam <thanikachalam.niranjan@gmail.com>
2025-02-28 08:51:51 +0100

    push to hf

---
+++ TinyStories/create_hf_dataset.py
@@ -0,0 +1,22 @@
+import os
+import json
+import datasets
+
+data_dir = "chunks"
+data = []
+
+for filename in os.listdir(data_dir):
+    if filename.endswith(".json"):
+        file_path = os.path.join(data_dir, filename)
+        print(f"Loading {file_path}")
+        with open(file_path, "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+            
+            for item in json_data:
+                for key, text in item.items(): 
+                    data.append({"row": int(key), "text": text.replace("\u200b", "")})
+data.sort(key=lambda x: x["row"])
+sorted_text_data = [{"text": item["text"]} for item in data]
+dataset = datasets.DatasetDict({"train":datasets.Dataset.from_list(sorted_text_data)})
+dataset.save_to_disk("tinystories_ta_google_translate")
+dataset.pushto_hub("tniranjan/tinystories_ta_google_translate")
